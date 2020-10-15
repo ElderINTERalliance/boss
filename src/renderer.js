0b553f80212526cd8b104cf9e90375fb9184914e
zsarge <zackarywsargent@gmail.com>
2020-10-14 20:48:02 -0400

    Add basic show/hide functionality
    Bug: There is still whitespace in the scrollbar

--- src/renderer.js
+++ src/renderer.js
@@ -1,15 +1,12 @@
-function goToHome() {
-   alert("home") 
-}
+goToElement("home");
 
-function goToTimer() {
-   alert("timer") 
-}
-
-function goToSchedule() {
-   alert("schedule") 
-}
-
-function goToApps() {
-   alert("apps") 
+function goToElement(name) {
+  let content = document.getElementsByClassName("content")[0];
+  let elements = content.getElementsByTagName("div");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.display = 'none';
+  }
+  let selected = document.getElementById(name);
+  selected.style.display = 'block';
+  window.scrollTo(0,0);
 }
