725d073f9c6c9752a48155984f0d457ab9823190
Kevin Long <kevin.uu.long@gmail.com>
2020-10-15 00:01:52 -0400

    Start UI Implementation
    I have started implementing the UI. The main view is almost done.
    
    I also did a little formatting and file structure reorganization.

--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,12 @@
 goToElement("home");
 
 function goToElement(name) {
-  let content = document.getElementsByClassName("content")[0];
-  let elements = content.getElementsByTagName("div");
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].style.display = 'none';
-  }
-  let selected = document.getElementById(name);
-  selected.style.display = 'block';
-  window.scrollTo(0,0);
+	let content = document.getElementsByClassName("content")[0];
+	let elements = content.getElementsByTagName("div");
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].style.display = 'none';
+	}
+	let selected = document.getElementById(name);
+	selected.style.display = 'block';
+	window.scrollTo(0, 0);
 }
