76f3e95879e699c6393f8ba77e515ef2eb288445
zsarge <zackarywsargent@gmail.com>
2020-10-15 23:09:51 -0400

    Highlight selected Navbar Item

--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,25 @@
-goToElement("home-view");
+let homeElement = document.getElementsByClassName("nav-item")[0];
+goToElement("home-view", homeElement);
 
-function goToElement(name) {
+function goToElement(name, navElement) {
+	// Hide all elements
 	let content = document.getElementById("content");
 	let elements = content.childNodes;
 	for (let i = 0; i < elements.length; i++) {
 		elements[i].hidden = true;
 	}
+
+	// Show selected content
 	let selected = document.getElementById(name);
-	selected.hidden = false
+	selected.hidden = false;
 	window.scrollTo(0, 0);
+
+	// Set default background color for all nav headers
+	sections = document.getElementsByClassName("nav-item");
+	Object.values(sections).forEach((element) => {
+		element.style.background = "var(--nav-default)";
+	});
+
+	// Highlight selected
+	navElement.style.background = "var(--nav-selected)";
 }
