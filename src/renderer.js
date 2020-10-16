290592513a786f6f0b149d088a0288f400730e35
zsarge <zackarywsargent@gmail.com>
2020-10-15 21:29:18 -0400

    Make formatting consistent
    - Intent using tabs
    - Display 4 spaces wide
    - Semicolons on
    - Braces on same line

--- src/renderer.js
+++ src/renderer.js
@@ -4,9 +4,9 @@ function goToElement(name) {
 	let content = document.getElementsByClassName("content")[0];
 	let elements = content.getElementsByTagName("div");
 	for (let i = 0; i < elements.length; i++) {
-		elements[i].style.display = 'none';
+		elements[i].style.display = "none";
 	}
 	let selected = document.getElementById(name);
-	selected.style.display = 'block';
+	selected.style.display = "block";
 	window.scrollTo(0, 0);
 }
