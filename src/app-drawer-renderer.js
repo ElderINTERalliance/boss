a62af44b5df513f84eae3f0822988a2472346a4a
zsarge <zackarywsargent@gmail.com>
2020-10-16 16:24:06 -0400

    Add elements to drawer
    I just realized the elements are just images, not images in a list.
    I'll fix that now

--- src/app-drawer-renderer.js
+++ src/app-drawer-renderer.js
@@ -4,6 +4,7 @@
 // const app = require("electron");
 const preferences = require("./preferences-handler");
 const storage = require("electron-json-storage");
+const electron = require("electron");
 const fs = require("fs");
 
 const path = storage.getDefaultDataPath();
@@ -22,22 +23,53 @@ function getUserPreferences() {
 }
 
 // Display User Data
-// This is run at script load
+// THIS IS RUN AT SCRIPT LOAD
 let drawer = document.getElementById("app-drawer");
-drawer.append(createIcons());
+drawer.append(parsePreferences(getUserPreferences()));
 
-// O(2n) complexity
-function createIcons(data) {
-	data = getUserPreferences();
+// Takes user preferences, and returns a list
+// of drawers of apps
+function parsePreferences(data) {
+	var root = document.createElement("div");
+	Object.keys(data).forEach((drawer) => {
+		root.append(createDrawer(drawer, data[drawer]));
+	});
+	return root;
+}
+
+function createDrawer(drawerName, drawerContents) {
+	let div = initializeDrawerElement();
+	let title = div.getElementsByClassName("shortcut-card-label")[0];
+	title.textContent = drawerName;
+	let body = div.getElementsByClassName("shortcut-card-programs")[0];
+	body.append(createListFromIcons(drawerContents));
+	return div;
+}
+
+function initializeDrawerElement() {
+	let div = document.createElement("div");
+	div.innerHTML += `
+	<div class="shortcut-card-home">
+		<div class="shortcut-card-icons">
+			<div class="icon" id="launch"><i class="material-icons-round md-48">launch</i></div>
+			<div class="icon" id="edit"><i class="material-icons-round md-48">edit</i></div>
+		</div>
+		<div class="shortcut-card-programs">
+		</div>
+		<h2 class="shortcut-card-label">
+		</h2>
+	</div>`;
+	return div;
+}
+
+function createListFromIcons(icons) {
 	var list = document.createElement("ul");
-	Object.values(data).forEach((drawer) => {
-		Object.keys(drawer).forEach((appName) => {
-			let app = drawer[appName];
-			let iconURL = app["icon"];
-			let src = app["src"];
-			let protocol = app["protocol"];
-			addApp(createIconElement(iconURL, appName), src, protocol, list);
-		});
+	Object.keys(icons).forEach((appName) => {
+		let app = icons[appName];
+		let iconURL = app["icon"];
+		let src = app["src"];
+		let protocol = app["protocol"];
+		addApp(createIconElement(iconURL, appName), src, protocol, list);
 	});
 	return list;
 }
@@ -46,14 +78,20 @@ function createIconElement(url, title = "") {
 	return `<img class="shortcut-card-program-icon" src="${url}" title="${title}"/>`;
 }
 
-function addApp(content, src, protocol, list) {
-	list.innerHTML += `<li> ${content} </li>`;
+function addApp(icon, src, protocol, list) {
+	list.innerHTML += `<li> ${icon} </li>`;
 	list.onclick = function () {
 		openApp(src, protocol);
 	};
 }
 
 function openApp(src, protocol) {
-	console.log(src);
-	console.log(protocol);
+	if (!protocol) return;
+	if (protocol == "browser") {
+		openUrlInBrowser(src);
+	}
+}
+
+function openUrlInBrowser(url) {
+	electron.shell.openExternal(url);
 }
