61b5d589b99ba520d926857fd25a28a559d4966e
Kevin Long <kevin.uu.long@gmail.com>
2020-10-17 16:35:40 -0400

    Finalize Edit View
    Probably ready to be merged to the main branch. Colors should now update and I believe all visual aspects should now be completed.

--- src/renderer.js
+++ src/renderer.js
@@ -13,6 +13,30 @@ function goToElement(name) {
 
 document.querySelectorAll(".edit-programs-list-icon").forEach(element => {
 	element.addEventListener('click', () => {
+		color = document.getElementById("edit-pane").dataset.color;
 		element.classList.toggle("program-selected");
+		element.classList.toggle(`light-${color}`)
+	})
+})
+
+// Color Switcher
+
+var color = document.getElementById("edit-pane").dataset.color;
+
+document.querySelectorAll(".large-color").forEach(element => {
+	if (element.dataset.color == color){
+		element.classList.toggle("color-selected");
+	}
+	element.addEventListener('click', () => {
+		document.querySelector(".color-selected").classList.toggle('color-selected');
+		element.classList.toggle("color-selected");
+		color = document.getElementById("edit-pane").dataset.color;
+		document.querySelectorAll(`.light-${color}:not(.lock)`).forEach(el => {
+			el.classList.replace(`light-${color}`, `light-${element.dataset.color}`);
+		})
+		document.querySelectorAll(`.dark-${color}:not(.lock)`).forEach(el => {
+			el.classList.replace(`dark-${color}`, `dark-${element.dataset.color}`);
+		})
+		document.getElementById("edit-pane").dataset.color = element.dataset.color;
 	})
 })
