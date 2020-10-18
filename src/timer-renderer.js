67b08b581f3252a8617f82344c47167eae2d2ae9
zsarge <zackarywsargent@gmail.com>
2020-10-18 12:57:00 -0400

    Add Timer Log

--- src/timer-renderer.js
+++ src/timer-renderer.js
@@ -2,6 +2,8 @@
 
 var totalMins = 24;
 var totalSecs = 59;
+var startMins = 0;
+var startSecs = 0;
 var mainLoop;
 
 function displayTime(mins, secs) {
@@ -47,6 +49,8 @@ function changeTime(mins, secs) {
 function setTime(mins, secs) {
 	totalMins = 0;
 	totalSecs = 0;
+	startSecs = 0;
+	startMins = 0;
 	changeTime(mins, secs);
 	displayTime(totalMins, totalSecs);
 }
@@ -68,6 +72,9 @@ function startTimer() {
 
 	let stopBtn = document.getElementById("stop-button");
 	stopBtn.className = "mdc-button mdc-button--raised";
+
+	startMins = totalMins;
+	startSecs = totalSecs;
 }
 
 function stopTimer() {
@@ -77,4 +84,16 @@ function stopTimer() {
 
 	let stopBtn = document.getElementById("stop-button");
 	stopBtn.className = "mdc-button mdc-button--outlined";
+
+	// frame.textContent = secsRunning;
+	log(startMins - totalMins, startSecs - totalSecs);
+}
+
+function log(mins, secs) {
+	if (mins < 0 || secs < 0) return
+	secs += 1 // account for instant decrease at timer start
+	let frame = document.getElementById("timer-log-frame")
+	let s = "s"
+	if (secs == 1) s = "" 
+	frame.innerHTML += `<p>${mins} minutes ${secs} second${s}</p>`
 }
